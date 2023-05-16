3c9ce04e96afb8d8951fd6bba3e1fcfd9d4bb036
ezi-s-u <ljsgoo0617@gmail.com>
2023-05-16 20:01:49 +0900

    Initial commit

---
+++ app/src/main/java/kr/hs/emirim/evie/compoundtest/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.hs.emirim.evie.compoundtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.image_test)
+    }
+}
